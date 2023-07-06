89480400de37907081512d18693b5b95b3088431
anastasija-kalpakovska <anastasija.kalpakovska@yahoo.com>
2023-07-07 01:16:13 +0200

    export/import news and events

--- JS/Like.js
+++ JS/Like.js
@@ -1,9 +1,9 @@
-let counterBTN = 0;
-let LIkes = document.getElementById("LIkes");
-likeBtn.addEventListener("click", () => {
-  LIkes.innerText = counter();
-});
-function counter() {
-  counterBTN++;
-  return counterBTN;
-}
+//  let counterBTN = 0;
+//  let LIkes = document.getElementById("LIkes");
+//  likeBtn.addEventListener("click", () => {
+//    LIkes.innerText = counter();
+//  });
+//  function counter() {
+//    counterBTN++;
+//    return counterBTN;
+// }
---
+++ JS/data.js
@@ -0,0 +1,263 @@
+
+  export let News = [
+     {
+       newsId: 0,
+       title: `Confusion at Twitter continues over Elon Musk's tweet limits`,
+       image: `https://ichef.bbci.co.uk/news/976/cpsprodpb/DC6B/production/_130272465_gettyimages-1240294770.jpg.webp`,
+      Description: `Confusion at Twitter appears to be continuing after owner Elon Musk introduced limits to the amount of posts users can read in a day.`,
+      URL: `https://www.bbc.com/news/technology-66087172`,
+      Date: `07-05-2023`,
+    },
+     {
+       newsId: 1,
+       title: `Threads: Instagram owner to launch Twitter rival on Thursday`,
+       image: `https://ichef.bbci.co.uk/news/976/cpsprodpb/1092F/production/_130278876_threads.jpg.webp`,
+       Description: `Facebook owner Meta is launching its new app to rival Twitter and says it will go live on Thursday.`,
+       URL: `https://www.bbc.com/news/business-66094072`,
+       Date: `07-05-2023`,
+    },
+    {
+      newsId: 2,
+      title: `Pokemon Go maker Niantic cuts a quarter of its workforce`,
+      image: `https://ichef.bbci.co.uk/news/976/cpsprodpb/6508/production/_130246852_fourpikachus.jpg.webp`,
+      Description: `The maker of the Pokemon Go video game, Niantic, says it will cut a quarter of its workforce as the gaming industry faces a slowdown in demand after downloads surged during the pandemic.`,
+      URL: `https://www.bbc.com/news/business-66062372`,
+       Date: `07-05-2023`,
+     },
+    {
+      newsId: 3,
+       title: `The AI trained to recognise waste for recycling`,
+       image: `https://ichef.bbci.co.uk/news/976/cpsprodpb/2C63/production/_130236311_gettyimages-1409469474.jpg.webp`,
+      Description: `There is a lot of rubbish in the world.`,
+       URL: `https://www.bbc.com/news/business-66042169`,
+      Date: `07-05-2023`,
+    },
+    {
+      newsId: 4,
+       title: `China curbs exports of key computer chip materials`,
+       image: `https://ichef.bbci.co.uk/news/976/cpsprodpb/D27F/production/_130278835_gettyimages-1413577536.jpg.webp`,
+       Description: `The Chinese government is tightening controls over exports of two key materials used to make computer chips.`,
+       URL: `https://www.bbc.com/news/business-66093114`,
+       Date: `07-05-2023`,
+     },
+   {
+       newsId: 5,
+       title: `Konux gears up to scale its AI + IoT play for optimizing the railways`,
+       image: `https://techcrunch.com/wp-content/uploads/2023/06/KONUX_UI_01.jpg?w=1390&crop=1`,
+       Description: `Unless you’ve been on an extended digital detox this year, you can’t have missed how a certain flavor of AI hype has been accelerating down the tracks like a runaway train.`,
+       URL: `https://techcrunch.com/2023/07/04/konux-db-expansion/`,
+       Date: `07-04-2023`,
+     },
+     {
+       newsId: 6,
+       title: `The remote work startups that will last aren’t actually remote work startups`,
+       image: `https://techcrunch.com/wp-content/uploads/2023/06/GettyImages-594830889.jpg?w=1390&crop=1`,
+       Description: `Back in 2020 when the pandemic forced people to work from home, entrepreneurs saw an opportunity to build tools to fuel this new transition to remote work. `,
+       URL: `https://techcrunch.com/2023/07/03/remote-work-startups-future/`,
+       Date: `07-03-2023`,
+     },
+     {
+       newsId: 7,
+       title: `AI is not a panacea for software development`,
+       image: `https://techcrunch.com/wp-content/uploads/2023/06/GettyImages-1213838977.jpg?w=1390&crop=1`,
+       Description: `How much more productive are developers using AI coding tools?`,
+       URL: `https://techcrunch.com/2023/06/30/ai-is-not-a-panacea-for-software-development/`,
+      Date: `06-30-2023`,
+     },
+     {
+       newsId: 8,
+       title: `How confidential computing could secure generative AI adoption`,
+      image: `https://techcrunch.com/wp-content/uploads/2023/06/GettyImages-1399061540.jpg?w=1390&crop=1`,
+       Description: `Generative AI has the potential to change everything. It can inform new products, companies, industries, and even economies.`,
+       URL: `https://techcrunch.com/2023/06/29/how-confidential-computing-could-secure-generative-ai-adoption/`,
+       Date: `06-29-2023`,
+     },
+     {
+       newsId: 9,
+      title: `Microsoft seems to think we’re getting a PS5 Slim this year`,
+       image: `https://duet-cdn.vox-cdn.com/thumbor/0x0:2040x1360/640x427/filters:focal(1020x680:1021x681):format(webp)/cdn.vox-cdn.com/uploads/chorus_asset/file/22015298/vpavic_4278_20201030_0120.jpg`,
+       Description: `Microsoft also thinks the handheld PlayStation will be ‘under $300’ when it launches later this year.`,
+      URL: `https://www.theverge.com/2023/7/4/23783621/microsoft-playstation-5-slim-model-release-date-pricing`,
+       Date: `07-04-2023`,
+     },
+     {
+       newsId: 10,
+      title: `It’s clearly time: all the news about the transparent tech renaissance`,
+       image: `https://duet-cdn.vox-cdn.com/thumbor/0x0:1600x1464/1920x1757/filters:focal(800x732:801x733):format(webp)/cdn.vox-cdn.com/uploads/chorus_asset/file/24763899/jsaux_rgb_back.jpg`,
+       Description: `Gadgets, much like fashion, can make style comebacks.`,
+       URL: `https://www.theverge.com/23774468/clear-transparent-tech-gadgets-news-announcements-products`,
+       Date: `07-05-2023`,
+     },
+   ];
+
+   export let events = [
+     {
+       eventsId: 0,
+       title: `Cisco Live`,
+       image: `https://www.ciscolive.com/c/dam/r/ciscolive/headlibs/img/global-event/logo-white-blue-2023.png`,
+       Description: `We hope you saw firsthand how we can explore possibilities to build a more inclusive future together.`,
+       URL: `https://www.ciscolive.com/global.html`,
+       Date: `06-02-2023 -> 06.06.2023`,
+       destination: `Las Vegas`
+     },
+     {
+       eventsId: 1,
+       title: `London Tech Week`,
+       image: `https://londontechweek.com/assets/media/LTW-logo-alt.f93e2c1f.svg`,
+       Description: `In 2024 London Tech Week is going BIG. With double the space, double the opportunity and double the impact at our new campus at London Olympia.`,
+       URL: `https://londontechweek.com/`,
+       Date: `06-10-2024->06-14-2024`,
+       destination: `London`
+     },
+     {
+       eventsId: 2,
+       title: `Rethink the power of infosecurity`,
+       image: `https://www.infosecurityeurope.com/content/dam/sitebuilder/rxuk/infeu/images/homepage/homepage-2/conference-homepage.jpg/_jcr_content/renditions/original.image_file.557.557.file/768825651/conference-homepage.jpg`,
+       Description: `Infosecurity Europe keeps you connected with everyone in information security. The brightest minds, from engineers to innovators.
+           We keep you up to date on everything about information security. From the past battles we’ve won to the present.
+       Join us as we rethink the power of infosecurity and discover what that may mean to you.`,
+       URL: `https://www.infosecurityeurope.com/en-gb.html`,
+     Date: `06-04-2023->06-06-2023`,
+       destination: `London`
+    },
+     {
+       eventsId: 3,
+       title: `See the best of Summit`,
+       image: `https://www.databricks.com/dataaisummit/static/logo-a28ef8a2cc8138ac665c1a46fb7b0482.svg`,
+       Description: `Watch all the keynotes, 250+ breakouts and more from the global event for the data and AI community — available until July 14.`,
+       URL: `https://www.databricks.com/dataaisummit/`,
+       Date: `06-10-2023->06-13-2023`,
+       destination: `San Francisco`,
+     },
+     {
+       eventsId: 4,
+       title: `Africa Automation Technology Fair`,
+       image: `https://www.alltechconferences.com/wp-content/uploads/2022/03/Africa-Automation-Technology-Fair.jpg`,
+       Description: `The industry is constantly transforming, so are we. Positioned as the heartbeat of the industry in Africa, Africa Automation Fair has evolved and transformed to meet the needs of the industry and is now named the Africa Automation Technology Fair.`,
+       URL: `https://www.alltechconferences.com/event/africa-automation-technology-fair/`,
+       Date: `05-09-2023->05-11-2023`,
+       destionation : `online`
+     },
+     {
+       eventsId: 5,
+       title: `MIPRO 2023`,
+       image: `https://www.alltechconferences.com/wp-content/uploads/2022/12/MIPRO2023logo1669992721-794x600.png`,
+       Description: `The 46th International ICT Convention MIPRO 2023 is traditionally dedicated to information, communication and electronic technology, smart industry, renewable energy sources, ecology along with new and high technologies in general.`,
+       URL: `https://www.alltechconferences.com/event/mipro-2023/`,
+       Date: `05-22-2023->05-26-2023`,
+       destination: `online`
+     },
+     {
+       eventsId: 6,
+       title: `CES 2023`,
+      image: `https://media.licdn.com/dms/image/D5612AQGSFt4siF0BIg/article-inline_image-shrink_1000_1488/0/1678303604769?e=1694044800&v=beta&t=_ueMleEjunpyVCOSBwtI7tms6zxugxYimwHRysefeEc`,
+       Description: `CES is a trade-only event for individuals 18 years of age or older and affiliated with the consumer technology industry.`,
+       URL: `https://registration.experientevent.com/ShowCES231/wizard/landing`,
+       Date: `01-05-2023->01-08-2023`,
+       destination: `Las Vegas`
+     },
+     {
+       eventsId: 7,
+       title: `Mobile World Congress (MWC)`,
+      image: `https://media.licdn.com/dms/image/D5612AQEcfrmkxfrYvA/article-inline_image-shrink_1000_1488/0/1678303664754?e=1694044800&v=beta&t=uT-n1UpCeeYNVXH0k9ynUGtgUWxX4R7argB4k_j9NOY`,
+       Description: `If you’re looking for startup conferences and the biggest tech conferences of the year, look no further than Mobile World Congress (MWC). MWC offers great value and is one of the best technology conferences out there. `,
+      URL: `https://www.mwcbarcelona.com/`,
+       Date: `02-27-2023->03-02-2023`,
+      destination: `Barcelona`
+     },
+     {
+       eventsId: 8,
+       title: ` SXSW`,
+      image: `https://media.licdn.com/dms/image/D5612AQGHvisyoIVNaQ/article-inline_image-shrink_1000_1488/0/1678303708698?e=1694044800&v=beta&t=atq0ho-9u9cU6w5nelxmA1mrUx3wq5DisHRDFTbarOY`,
+       Description: `Head on down to Austin for the biggest startup conference of the year – SXSW (aka “South by Southwest”)! `,
+      URL: `https://www.sxsw.com/conference/`,
+       Date: `03-10-2023->03-19-2023`,
+       destination: `Austin, Texas`
+     },
+     {
+       eventsId: 9,
+      title: `Finovate Europe 2023`,
+       image: `https://knect365.imgix.net/uploads/FE2021V1-large-fb58822d1d7a5c9eb354332bf2aa9b82.png?auto=format&fit=max&h=80&dpr=1`,
+       Description: ``,
+       URL: `https://informaconnect.com/finovateeurope/`,
+       Date: `03-14-2023->03-15-2023`,
+       destination: `London`
+    },
+     {
+      eventsId: 10,
+       title: `RSA Conference 2023`,
+       image: `https://media.licdn.com/dms/image/D5612AQHErer4kMHYyA/article-inline_image-shrink_1000_1488/0/1678303808661?e=1694044800&v=beta&t=9Dmnjlrxbo0CtdEdo8iUT_EFI3h80EQ7lGYQw5jcbbI`,
+       Description: `RSAC 2023 brought together thousands of cybersecurity professionals for four incredible days of expert perspectives, groundbreaking innovation, and best practices. Now, you can explore all the best moments, watch stand-out Keynotes, and more!`,
+       URL: `https://www.rsaconference.com/usa`,
+       Date: `04-23-2023->04-27-2023`,
+       destination: `San Francisco`
+     }
+   ];
+ 
+   export function cardTechCrunch(data) {
+     return `
+         <div class="post-box">
+       <img id="data.newsId" onclick="fullboxCard(${data.newsId}) " src="${data.image}" class="post-img" alt="..." />
+       <div class="datas">
+         <a  href="fullBox.html" class="post-title"> ${data.title}</a>
+         <span class="post-date">${data.Date}</span>
+         <p class="post-description">
+        ${data.Description}
+        </p>
+         <div class="source-name">
+          <span class="source-name">BBC</span>
+        </div>
+         <div class="likes">
+         <div class="likes-up">
+           <button onclick="likeCounterFn(${data.newsId})">
+             <i class="fa fa-thumbs-up"></i>
+           </button>
+           <span id="likes_${data.newsId}">0</span>
+         </div>
+           <div class="likes-down">
+           <button onclick="unlikeCounterFn(${data.newsId})">
+            <i class="fa fa-thumbs-down"></i>
+           </button>
+           <span id="unlikes_${data.newsId}">0</span>
+         </div>
+       </div>
+       </div>
+     </div>
+     `;
+   }
+  
+ export function cardEvents(data) {
+     return `
+          <div class="post-box">
+       <img id="data.eventsId" onclick="fullboxCard(${data.eventsId}) " src="${data.image}" class="post-img" alt="..." />
+       <div class="datas">
+         <a  href="fullBox.html" class="post-title"> ${data.title}</a>
+         <span class="post-date">${data.Date}</span>
+         <p class="post-description">
+         ${data.Description}
+         </p>
+         <div class="source-name">
+           <span class="source-name">BBC</span>
+        </div>
+         <div class="likes">
+         <div class="likes-up">
+           <button onclick="likeCounterFn(${data.eventsId})">
+             <i class="fa fa-thumbs-up"></i>
+           </button>
+           <span id="likes_${data.eventsId}">0</span>
+         </div>
+           <div class="likes-down">
+           <button onclick="unlikeCounterFn(${data.eventsId})">
+             <i class="fa fa-thumbs-down"></i>
+           </button>
+           <span id="unlikes_${data.eventsId}">0</span>
+         </div>
+       </div>
+       </div>
+     </div>
+     `;
+   }
+  
+
+
+
